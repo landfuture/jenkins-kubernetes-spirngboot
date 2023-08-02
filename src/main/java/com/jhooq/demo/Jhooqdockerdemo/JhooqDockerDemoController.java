72689ba6e6f9034b0f608b91ab0785e74bc8fae1
landfuture <masterlandfuture@gmail.com>
2023-08-02 17:27:57 -0400

    555

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Hello Jhooq -cjsss ";
+        return "Docker Demo - Hello Jhooq -cjsss --- 2 ";
     }
 }
